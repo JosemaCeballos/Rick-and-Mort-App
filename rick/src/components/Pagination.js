e22a57f6c7bd4b956a43181b19d61c42658ad799
JosemaCeballos <josema.soyhenry@gmail.com>
2022-09-18 22:16:11 -0300

    Practica - Rick and Morty App

---
+++ rick/src/components/Pagination.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Pagination = ({ prev, next, onPrevious, onNext }) => {
+  const handlePrevios = () => {
+    onPrevious();
+  };
+
+  const handleNext = () => {
+    onNext();
+  };
+
+  return (
+    <nav className="my-5">
+      <ul className="pagination justify-content-center">
+        {prev ? (
+          <li className="page-item">
+            <button className="page-link" onClick={handlePrevios}>
+              Previous
+            </button>
+          </li>
+        ) : null}
+        {next ? (
+          <li className="page-item">
+            <button className="page-link" onClick={handleNext}>
+              Next
+            </button>
+          </li>
+        ) : null}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
